c746397068bdf7cd60fc787038d2b26ce9a0b5ec
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-22 11:13:00 +0000

    add event listener called loveMe

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const loveMe = document.querySelector(".loveMe");
 const times = document.querySelector("#times");
+
+let clickTime = 0;
+
+loveMe.addEventListener("click", (e) => {
+  console.log(123);
+});
